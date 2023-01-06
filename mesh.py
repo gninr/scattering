8d98bc973c7ec572b1e1330d8ede16bf96a152cf
Ning Ren <ninren@student.ethz.ch>
2023-01-06 22:49:29 +0100

    solver for direct problem

--- mesh.py
+++ mesh.py
@@ -3,7 +3,7 @@ import numpy as np
 import firedrake as fd
 
 
-def generate_mesh(a0, a1, b0, b1, shape, h0, level=0):
+def generate_mesh(a0, a1, b0, b1, shape, h0, level=0, R0=None, R1=None):
     gmsh.initialize()
 
     # Absorbing layer
@@ -12,20 +12,44 @@ def generate_mesh(a0, a1, b0, b1, shape, h0, level=0):
     gmsh.model.occ.addPoint(-a0, b0, 0, 1, 3)
     gmsh.model.occ.addPoint(-a0, -b0, 0, 1, 4)
     gmsh.model.occ.addPoint(a1, -b1, 0, 1, 5)
-    gmsh.model.occ.addPoint(a1, b1, 0, 1, 6)
-    gmsh.model.occ.addPoint(-a1, b1, 0, 1, 7)
-    gmsh.model.occ.addPoint(-a1, -b1, 0, 1, 8)
-    kp_start = 9
+    gmsh.model.occ.addPoint(a1, -b0, 0, 1, 6)
+    gmsh.model.occ.addPoint(a1, b0, 0, 1, 7)
+    gmsh.model.occ.addPoint(a1, b1, 0, 1, 8)
+    gmsh.model.occ.addPoint(a0, b1, 0, 1, 9)
+    gmsh.model.occ.addPoint(-a0, b1, 0, 1, 10)
+    gmsh.model.occ.addPoint(-a1, b1, 0, 1, 11)
+    gmsh.model.occ.addPoint(-a1, b0, 0, 1, 12)
+    gmsh.model.occ.addPoint(-a1, -b0, 0, 1, 13)
+    gmsh.model.occ.addPoint(-a1, -b1, 0, 1, 14)
+    gmsh.model.occ.addPoint(-a0, -b1, 0, 1, 15)
+    gmsh.model.occ.addPoint(a0, -b1, 0, 1, 16)
+    kp_start = 17
 
     gmsh.model.occ.addLine(1, 2, 1)
     gmsh.model.occ.addLine(2, 3, 2)
     gmsh.model.occ.addLine(3, 4, 3)
     gmsh.model.occ.addLine(4, 1, 4)
-    gmsh.model.occ.addLine(5, 6, 5)
-    gmsh.model.occ.addLine(6, 7, 6)
-    gmsh.model.occ.addLine(7, 8, 7)
-    gmsh.model.occ.addLine(8, 5, 8)
-    kc_start = 9
+    gmsh.model.occ.addLine(6, 7, 5)
+    gmsh.model.occ.addLine(9, 10, 6)
+    gmsh.model.occ.addLine(12, 13, 7)
+    gmsh.model.occ.addLine(15, 16, 8)
+    gmsh.model.occ.addLine(2, 7, 9)
+    gmsh.model.occ.addLine(7, 8, 10)
+    gmsh.model.occ.addLine(8, 9, 11)
+    gmsh.model.occ.addLine(9, 2, 12)
+    gmsh.model.occ.addLine(3, 10, 13)
+    gmsh.model.occ.addLine(10, 11, 14)
+    gmsh.model.occ.addLine(11, 12, 15)
+    gmsh.model.occ.addLine(12, 3, 16)
+    gmsh.model.occ.addLine(4, 13, 17)
+    gmsh.model.occ.addLine(13, 14, 18)
+    gmsh.model.occ.addLine(14, 15, 19)
+    gmsh.model.occ.addLine(15, 4, 20)
+    gmsh.model.occ.addLine(1, 16, 21)
+    gmsh.model.occ.addLine(16, 5, 22)
+    gmsh.model.occ.addLine(5, 6, 23)
+    gmsh.model.occ.addLine(6, 1, 24)
+    kc_start = 25
 
     # Obstacle
     if shape == "sphere":
@@ -54,21 +78,57 @@ def generate_mesh(a0, a1, b0, b1, shape, h0, level=0):
         print("Unsupported shape.")
         raise NotImplementedError
 
-    gmsh.model.occ.addCurveLoop(range(kc_start, kc_end), 1)
-    gmsh.model.occ.addCurveLoop(range(1, 5), 2)
-    gmsh.model.occ.addCurveLoop(range(5, 9), 3)
+    gmsh.model.occ.addCurveLoop(range(1, 5), 1)
+    gmsh.model.occ.addCurveLoop([5, -9, -1, -24], 2)
+    gmsh.model.occ.addCurveLoop([-12, 6, -13, -2], 3)
+    gmsh.model.occ.addCurveLoop([-3, -16, 7, -17], 4)
+    gmsh.model.occ.addCurveLoop([-21, -4, -20, 8], 5)
+    gmsh.model.occ.addCurveLoop(range(9, 13), 6)
+    gmsh.model.occ.addCurveLoop(range(13, 17), 7)
+    gmsh.model.occ.addCurveLoop(range(17, 21), 8)
+    gmsh.model.occ.addCurveLoop(range(21, 25), 9)
+    gmsh.model.occ.addCurveLoop(range(kc_start, kc_end), 10)
 
-    gmsh.model.occ.addPlaneSurface([1, 2], 1)
-    gmsh.model.occ.addPlaneSurface([2, 3], 2)
+    for cl in range(1, 9):
+        gmsh.model.occ.addPlaneSurface([cl + 1], cl)
+
+    if R0 is None:
+        gmsh.model.occ.addPlaneSurface([1, 10], 9)
+    else:
+        circ0 = gmsh.model.occ.addCircle(0, 0, 0, R0)
+        cl0 = gmsh.model.occ.addCurveLoop([circ0])
+        gmsh.model.occ.addPlaneSurface([10, cl0], 9)
+        if R1 is None:
+            gmsh.model.occ.addPlaneSurface([cl0, 1], 10)
+        else:
+            circ1 = gmsh.model.occ.addCircle(0, 0, 0, R1)
+            cl1 = gmsh.model.occ.addCurveLoop([circ1])
+            gmsh.model.occ.addPlaneSurface([cl0, cl1], 10)
+            gmsh.model.occ.addPlaneSurface([cl1, 1], 11)
 
     gmsh.model.occ.synchronize()
 
     gmsh.model.addPhysicalGroup(1, range(kc_start, kc_end), 1, name="Gamma")
     gmsh.model.addPhysicalGroup(1, range(1, 5), 2, name="Gamma_I")
-    gmsh.model.addPhysicalGroup(1, range(5, 9), 3, name="Gamma_D")
+    gmsh.model.addPhysicalGroup(
+        1, list(range(5, 9)) + [10, 11, 14, 15, 18, 19, 22, 23], 3,
+        name="Gamma_D")
+    if R0 is not None:
+        gmsh.model.addPhysicalGroup(1, [circ0], 4, name="R0")
+    if R1 is not None:
+        gmsh.model.addPhysicalGroup(1, [circ1], 5, name="R1")
 
-    gmsh.model.addPhysicalGroup(2, [1], 1, name="Omega_F")
-    gmsh.model.addPhysicalGroup(2, [2], 2, name="Omega_A")
+    if R0 is None:
+        gmsh.model.addPhysicalGroup(2, [9], 1, name="Omega_F")
+    elif R1 is None:
+        gmsh.model.addPhysicalGroup(2, [9, 10], 1, name="Omega_F")
+    else:
+        gmsh.model.addPhysicalGroup(2, [9, 11], 1, name="Omega_F")
+        gmsh.model.addPhysicalGroup(2, [10], 5, name="Omega_inf")
+
+    gmsh.model.addPhysicalGroup(2, [1, 3], 2, name="Omega_A_x")
+    gmsh.model.addPhysicalGroup(2, [2, 4], 3, name="Omega_A_y")
+    gmsh.model.addPhysicalGroup(2, range(5, 9), 4, name="Omega_A_xy")
 
     gmsh.option.setNumber("Mesh.MeshSizeFactor", h0 / 2**level)
     gmsh.model.mesh.generate(2)
--- scattering.py
+++ scattering.py
@@ -3,31 +3,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def solve(mesh, k, a0, a1, b0, b1, g, neumann=False, quad_deg=None, beta=None):
+def solve(mesh, k, a0, a1, b0, b1, g, neumann=False,
+          quad_deg=None, beta=None, annular=False):
     # Function space
     W = fd.VectorFunctionSpace(mesh, "CG", 1)
-    if neumann:
-        p = fd.TrialFunction(W)
-    else:
-        p = fd.Function(W)
+    p = fd.TrialFunction(W)
     q = fd.TestFunction(W)
+    solution = fd.Function(W, name="sol")
 
     # Coefficient functions
     X = fd.SpatialCoordinate(mesh)
-    tol = 1e-10
     if beta is None:
         beta = 1
-    sigma_x = fd.conditional(abs(X[0]) > a0 - tol,
-                             beta / k / (a1 - abs(X[0])),
-                             0)
-    sigma_y = fd.conditional(abs(X[1]) > b0 - tol,
-                             beta / k / (b1 - abs(X[1])),
-                             0)
-    c1 = ((1 + sigma_x * sigma_y) / (1 + sigma_x**2),
-          (sigma_y - sigma_x) / (1 + sigma_x**2))
-    c2 = ((1 + sigma_x * sigma_y) / (1 + sigma_y**2),
-          (sigma_x - sigma_y) / (1 + sigma_y**2))
-    c3 = (1 - sigma_x * sigma_y, sigma_x + sigma_y)
+    sigma_x = beta / k / (a1 - abs(X[0]))
+    sigma_y = beta / k / (b1 - abs(X[1]))
+    c1x = (1 / (1 + sigma_x**2), -sigma_x / (1 + sigma_x**2))
+    c2x = (1, sigma_x)
+    c3x = (1, sigma_x)
+    c1y = (1, sigma_y)
+    c2y = (1 / (1 + sigma_y**2), -sigma_y / (1 + sigma_y**2))
+    c3y = (1, sigma_y)
+    c1xy = ((1 + sigma_x * sigma_y) / (1 + sigma_x**2),
+            (sigma_y - sigma_x) / (1 + sigma_x**2))
+    c2xy = ((1 + sigma_x * sigma_y) / (1 + sigma_y**2),
+            (sigma_x - sigma_y) / (1 + sigma_y**2))
+    c3xy = (1 - sigma_x * sigma_y, sigma_x + sigma_y)
 
     # Boundary data
     if neumann:
@@ -37,50 +37,55 @@ def solve(mesh, k, a0, a1, b0, b1, g, neumann=False, quad_deg=None, beta=None):
                fd.DirichletBC(W, (0., 0.), 3)]
 
     # Bilinear form
+    if annular:
+        Omega_F = fd.dx(1) + fd.dx(5)
+    else:
+        Omega_F = fd.dx(1)
     px, py = p.dx(0), p.dx(1)
     qx, qy = q.dx(0), q.dx(1)
-    a = (prod2(px, qx) + prod2(py, qy) - k**2 * prod2(p, q)) * fd.dx(1) \
-        + (prod3(c1, px, qx) + prod3(c2, py, qy)
-            - k**2 * prod3(c3, p, q)) * fd.dx(2)
+    a = (inner(px, qx) + inner(py, qy) - k**2 * inner(p, q)) * Omega_F\
+        + (inner(px, qx, c=c1x) + inner(py, qy, c=c2x)
+            - k**2 * inner(p, q, c=c3x)) * fd.dx(2)\
+        + (inner(px, qx, c=c1y) + inner(py, qy, c=c2y)
+            - k**2 * inner(p, q, c=c3y)) * fd.dx(3)\
+        + (inner(px, qx, c=c1xy) + inner(py, qy, c=c2xy)
+            - k**2 * inner(p, q, c=c3xy)) * fd.dx(4)
 
     # Linear form
-    if neumann:
-        L = (prod2(g, q)) * fd.ds(1)
-        p = fd.Function(W)
-    else:
-        L = 0
+    L = (inner(g, q)) * fd.ds(1) if neumann else 0
 
     # Solution
     fcp = {}
     if quad_deg is not None:
         fcp["quadrature_degree"] = quad_deg
-    fd.solve(a == L, p, bcs=bcs, form_compiler_parameters=fcp)
-    return p
+    problem = fd.LinearVariationalProblem(
+        a, L, solution, bcs, form_compiler_parameters=fcp)
+    solver = fd.LinearVariationalSolver(problem)
+    solver.solve()
+    return solution
 
 
-def prod2(x, y, split=False):
+def inner(x, y, c=None):
+    x_re, x_im = x
+    y_re, y_im = y
+    if c:
+        c_re, c_im = c
+        res_re = c_re * x_re * y_re + c_re * x_im * y_im + c_im * x_re * y_im\
+            - c_im * x_im * y_re
+        res_im = -c_re * x_re * y_im + c_re * x_im * y_re + c_im * x_re * y_re\
+            + c_im * x_im * y_im
+    else:
+        res_re = x_re * y_re + x_im * y_im
+        res_im = -x_re * y_im + x_im * y_re
+    return res_re + res_im
+
+
+def dot(x, y):
     x_re, x_im = x
     y_re, y_im = y
     res_re = x_re * y_re - x_im * y_im
     res_im = x_re * y_im + x_im * y_re
-    if split:
-        return res_re, res_im
-    else:
-        return res_re + res_im
-
-
-def prod3(x, y, z, split=False):
-    x_re, x_im = x
-    y_re, y_im = y
-    z_re, z_im = z
-    res_re = x_re * y_re * z_re - x_re * y_im * z_im - x_im * y_re * z_im \
-        - x_im * y_im * z_re
-    res_im = x_re * y_re * z_im + x_re * y_im * z_re + x_im * y_re * z_re \
-        - x_im * y_im * z_im
-    if split:
-        return res_re, res_im
-    else:
-        return res_re + res_im
+    return res_re, res_im
 
 
 def compute_error(u, uh, relative=True, norm="l2",
@@ -97,7 +102,7 @@ def compute_error(u, uh, relative=True, norm="l2",
             return fd.inner(h.dx(0), h.dx(0)) + fd.inner(h.dx(1), h.dx(1))
     elif norm == "h1":
         def form(h):
-            return fd.inner(h, h) \
+            return fd.inner(h, h)\
                 + fd.inner(h.dx(0), h.dx(0)) + fd.inner(h.dx(1), h.dx(1))
 
     err = fd.assemble(form(u - uh) * fd.dx(1),
@@ -108,23 +113,47 @@ def compute_error(u, uh, relative=True, norm="l2",
     return err
 
 
-def far_field(k, u, theta, inc=None):
-    mesh = u.function_space().mesh()
-    n = fd.FacetNormal(mesh)
-    y = fd.SpatialCoordinate(mesh)
-
+def far_field(k, u_s, theta, inc=None, boundary=1):
+    mesh = u_s.function_space().mesh()
     x = fd.Constant((np.cos(theta), np.sin(theta)))
+    y = fd.SpatialCoordinate(mesh)
+    n = fd.FacetNormal(mesh)
+    u = u_s + inc if inc else u_s
+
     phi = fd.pi / 4 - k * fd.inner(x, y)
     f = (fd.cos(phi), fd.sin(phi))
-    if inc is None:
+    if boundary == 1:
         g = (fd.inner(k * u[1] * x - fd.grad(u[0]), -n),
              fd.inner(-k * u[0] * x - fd.grad(u[1]), -n))
+        h = dot(f, g)
+        res_re = 1 / np.sqrt(8*np.pi*k) * fd.assemble(h[0] * fd.ds(1))
+        res_im = 1 / np.sqrt(8*np.pi*k) * fd.assemble(h[1] * fd.ds(1))
     else:
-        g = (fd.inner(k * (u[1] + inc[1]) * x - fd.grad(u[0] + inc[0]), -n),
-             fd.inner(-k * (u[0] + inc[0]) * x - fd.grad(u[1] + inc[1]), -n))
-    h = prod2(f, g, split=True)
-    res_re = 1 / np.sqrt(8*np.pi*k) * fd.assemble(h[0] * fd.ds(1))
-    res_im = 1 / np.sqrt(8*np.pi*k) * fd.assemble(h[1] * fd.ds(1))
+        g = (fd.inner(k * u[1] * x - fd.avg(fd.grad(u[0])), n('+')),
+             fd.inner(-k * u[0] * x - fd.avg(fd.grad(u[1])), n('+')))
+        h = dot(f, g)
+        res_re = 1 / np.sqrt(8*np.pi*k) * fd.assemble(h[0] * fd.dS(boundary))
+        res_im = 1 / np.sqrt(8*np.pi*k) * fd.assemble(h[1] * fd.dS(boundary))
+    return res_re, res_im
+
+
+def far_field_vol(k, u_s, theta, R0, R1, inc=None):
+    mesh = u_s.function_space().mesh()
+    x = fd.Constant((np.cos(theta), np.sin(theta)))
+    y = fd.SpatialCoordinate(mesh)
+    r = fd.sqrt(fd.dot(y, y))
+    psi = (1 - fd.cos((r - R0) / (R1 - R0) * fd.pi)) / 2
+    u = u_s + inc if inc else u_s
+
+    fcp = {"quadrature_degree": 4}
+    phi = fd.pi / 4 - k * fd.inner(x, y)
+    f = (fd.cos(phi), fd.sin(phi))
+    g = dot(u, (fd.div(fd.grad(psi)), -2 * k * fd.dot(x, fd.grad(psi))))
+    h = dot(f, g)
+    res_re = 1 / np.sqrt(8*np.pi*k) *\
+        fd.assemble(h[0] * fd.dx(5), form_compiler_parameters=fcp)
+    res_im = 1 / np.sqrt(8*np.pi*k) *\
+        fd.assemble(h[1] * fd.dx(5), form_compiler_parameters=fcp)
     return res_re, res_im
 
 
@@ -158,11 +187,11 @@ def plot_field(u, a0, a1, b0, b1):
     fig.colorbar(plot2, shrink=0.5, ax=ax2)
 
 
-def plot_far_field(k, u, inc=None):
+def plot_far_field(k, u, inc=None, boundary=1):
     theta = np.linspace(0, 2 * np.pi, 100)
     u_inf = []
     for t in list(theta):
-        u_inf.append(far_field(k, u, t, inc=inc))
+        u_inf.append(far_field(k, u, t, inc=inc, boundary=boundary))
     u_inf = np.array(u_inf)
 
     fig, (ax1, ax2) = plt.subplots(1, 2, subplot_kw={'projection': 'polar'},
@@ -175,3 +204,24 @@ def plot_far_field(k, u, inc=None):
     ax2.set_title("Imaginary part")
     ax2.set_rlabel_position(90)
     ax2.grid(True)
+    fig.suptitle("Boundary-based formula")
+
+
+def plot_far_field_vol(k, u, R0, R1, inc=None):
+    theta = np.linspace(0, 2 * np.pi, 100)
+    u_inf = []
+    for t in list(theta):
+        u_inf.append(far_field_vol(k, u, t, R0, R1, inc=inc))
+    u_inf = np.array(u_inf)
+
+    fig, (ax1, ax2) = plt.subplots(1, 2, subplot_kw={'projection': 'polar'},
+                                   constrained_layout=True)
+    ax1.plot(theta, u_inf[:, 0])
+    ax1.set_title("Real part")
+    ax1.set_rlabel_position(90)
+    ax1.grid(True)
+    ax2.plot(theta, u_inf[:, 1])
+    ax2.set_title("Imaginary part")
+    ax2.set_rlabel_position(90)
+    ax2.grid(True)
+    fig.suptitle("Volume-based formula")
